93ced4b7c9ea65efab96ec8856709a4971d477d2
CheesyChocolate <dev@behnamlal.xyz>
2023-11-05 22:41:46 +0000

    write a function to read tsp data from a file
    * also add a unit test for the function

---
+++ src/module/file_reader.py
@@ -0,0 +1,50 @@
+# This module is responsible for reading TSP data from files and storing it in
+# a suitable data structure.
+# The data structure used is a dictionary with the following keys:
+#   - 'NAME': name of the problem
+#   - 'COMMENT': comment about the problem
+#   - 'TYPE': type of problem (TSP)
+#   - 'DIMENSION': number of cities
+#   - 'EDGE_WEIGHT_TYPE': type of distance between cities (EUC_2D or ATT)
+#   - 'NODE_COORD_SECTION': list of coordinates of cities
+#       - each element of the list is a list with the following elements:
+#           - city ID   x coordinate  y coordinate
+
+def read_tsp_file(file_path):
+    tsp_data = {
+        'NAME': '',
+        'COMMENT': '',
+        'TYPE': '',
+        'DIMENSION': 0,
+        'EDGE_WEIGHT_TYPE': '',
+        'NODE_COORD_SECTION': []
+    }
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        reading_nodes = False
+
+        for line in lines:
+            line = line.strip()
+            if line.startswith('NAME'):
+                tsp_data['NAME'] = line.split(':')[1].strip()
+            elif line.startswith('COMMENT'):
+                tsp_data['COMMENT'] = line.split(':')[1].strip()
+            elif line.startswith('TYPE'):
+                tsp_data['TYPE'] = line.split(':')[1].strip()
+            elif line.startswith('DIMENSION'):
+                tsp_data['DIMENSION'] = int(line.split(':')[1].strip())
+            elif line.startswith('EDGE_WEIGHT_TYPE'):
+                tsp_data['EDGE_WEIGHT_TYPE'] = line.split(':')[1].strip()
+            elif line.startswith('NODE_COORD_SECTION'):
+                reading_nodes = True
+            elif line.startswith('EOF'):
+                break
+            elif reading_nodes:
+                city_data = line.split()
+                city_id = int(city_data[0])
+                x = float(city_data[1])
+                y = float(city_data[2])
+                tsp_data['NODE_COORD_SECTION'].append([city_id, x, y])
+
+    return tsp_data
---
+++ tests/test_file_reader.py
@@ -0,0 +1,24 @@
+import unittest
+from src.module.file_reader import read_tsp_file
+
+
+class TestFileReader(unittest.TestCase):
+    def test_read_tsp_file(self):
+        file_path = 'data/berlin52.tsp'
+        tsp_data = read_tsp_file(file_path)
+
+        self.assertEqual(tsp_data['NAME'], 'berlin52')
+        self.assertEqual(tsp_data['COMMENT'], '52 locations in Berlin (Groetschel)')
+        self.assertEqual(tsp_data['TYPE'], 'TSP')
+        self.assertEqual(tsp_data['DIMENSION'], 52)
+        self.assertEqual(tsp_data['EDGE_WEIGHT_TYPE'], 'EUC_2D')
+        self.assertEqual(len(tsp_data['NODE_COORD_SECTION']), 52)
+
+        # Verify the coordinates of a few cities
+        self.assertEqual(tsp_data['NODE_COORD_SECTION'][0], [1, 565.0, 575.0])
+        self.assertEqual(tsp_data['NODE_COORD_SECTION'][1], [2, 25.0, 185.0])
+        self.assertEqual(tsp_data['NODE_COORD_SECTION'][2], [3, 345.0, 750.0])
+
+
+if __name__ == '__main__':
+    unittest.main()
