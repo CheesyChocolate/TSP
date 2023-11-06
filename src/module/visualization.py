7bcbdb4e41f83153b4b936be6f8e2331749308f0
CheesyChocolate <dev@behnamlal.xyz>
2023-11-06 20:00:45 +0000

    `plot_tsp_cities` now gets a dictionary of city coordinates instead of a list of cities
    the plot for conecction also work now.

--- src/module/visualization.py
+++ src/module/visualization.py
@@ -5,20 +5,20 @@ import matplotlib.pyplot as plt
 
 
 # @param tsp_data: type tsp_data dict
-# @param connections: type 2D list
-def plot_tsp_cities(tsp_data, connections=None):
-    cities = tsp_data['NODE_COORD_SECTION']
-    x = [city[1] for city in cities]
-    y = [city[2] for city in cities]
+# @param connections: type list
+def plot_tsp_cities(tsp_data, solution=None):
+    cities = tsp_data
+    x = [city[0] for city in cities.values()]
+    y = [city[1] for city in cities.values()]
 
     plt.figure(figsize=(8, 6))
     plt.scatter(x, y, color='blue', marker='o', label='Cities')
 
-    if connections is not None:
-        for connection in connections:
-            plt.plot([x[connection[0]], x[connection[1]]],
-                     [y[connection[0]], y[connection[1]]],
-                     color='red', linestyle='-', linewidth=1, alpha=0.7)
+    # If a solution is provided, plot the solution path
+    if solution:
+        solution_x = [cities[city_id][0] for city_id in solution]
+        solution_y = [cities[city_id][1] for city_id in solution]
+        plt.plot(solution_x, solution_y, color='red', linestyle='-', linewidth=1, alpha=0.7, label='Solution Path')
 
     plt.xlabel('X Coordinate')
     plt.ylabel('Y Coordinate')
--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -4,15 +4,33 @@ from src.module.visualization import plot_tsp_cities
 
 
 class TestVisualization(unittest.TestCase):
-    def test_plot_tsp_cities(self):
-        tsp_data = {
-            'NODE_COORD_SECTION': [(1, 565.0, 575.0), (2, 25.0, 185.0), (3, 345.0, 750.0)]
+    def setUp(self):
+        # Sample NODE_COORD_SECTION dictionary
+        self.tsp_data = {
+            1: (565.0, 575.0),
+            2: (25.0, 185.0),
+            3: (345.0, 750.0),
+            4: (745.0, 225.0),
+            5: (475.0, 475.0)
         }
 
-        with patch('src.module.visualization.plt.show') as mock_show:
-            plot_tsp_cities(tsp_data)
+        # Sample solution (city IDs in a specific order)
+        self.solution = [1, 3, 2, 5, 4]
 
-        # Assert that plt.show() was called
+    @patch('src.module.visualization.plt.show')  # Mocking plt.show() method
+    def test_plot_tsp_cities_with_solution(self, mock_show):
+        # Test plotting cities with a solution
+        plot_tsp_cities(self.tsp_data, self.solution)
+
+        # Assert that plt.show() was called once
+        mock_show.assert_called_once()
+
+    @patch('src.module.visualization.plt.show')  # Mocking plt.show() method
+    def test_plot_tsp_cities_without_solution(self, mock_show):
+        # Test plotting cities without a solution
+        plot_tsp_cities(self.tsp_data)
+
+        # Assert that plt.show() was called once
         mock_show.assert_called_once()
 
 
