8b8d8dec933f75520de6d57db8478501bb99f0f3
CheesyChocolate <dev@behnamlal.xyz>
2023-11-15 22:18:56 +0000

    add sort_and_select_top_solutions function to genetic_operations module
    add it's test case to test_genetic_operations.py

--- src/module/genetic_operations.py
+++ src/module/genetic_operations.py
@@ -1,8 +1,10 @@
 # This module contains functions for generating chromosome to the TSP problem.
 # suppoted algorithms:
 #   - random chromosome
+#   - sort and select top solutions
 
 import random
+from .data_calculator import calculate_total_distance
 
 
 # @param tsp_data: Type dict (NODE_COORD_SECTION)
@@ -14,3 +16,29 @@ def generate_random_chromosome(tsp_data):
     # Ensure key 1 appears both at the beginning and the end of the chromosome
     chromosome = [1] + cities + [1]
     return chromosome
+
+
+# @param choromosomes: Type 2D list (chromosome: Type list)
+# @param tsp_data: Type dict (NODE_COORD_SECTION)
+# @param percentage: Type Int
+# @return: new_chromosomes: Type 2D list
+def sort_and_select_top_solutions(chromosomes, tsp_data, percentage):
+    # Calculate fitness scores for each chromosome
+    fitness_scores = [
+        calculate_total_distance(chromosome, tsp_data['NODE_COORD_SECTION'])
+        for chromosome in chromosomes
+    ]
+
+    # Combine chromosomes with their respective fitness scores
+    combined_data = list(zip(chromosomes, fitness_scores))
+
+    # Sort chromosomes based on fitness scores
+    sorted_chromosomes = sorted(combined_data, key=lambda x: x[1])  # Sort based on fitness scores
+
+    # Calculate the number of top solutions to select based on the percentage
+    num_selected = int(len(chromosomes) * percentage / 100)
+
+    # Get the top solutions
+    top_solutions = [solution[0] for solution in sorted_chromosomes[:num_selected]]
+
+    return top_solutions
--- tests/test_genetic_operations.py
+++ tests/test_genetic_operations.py
@@ -1,8 +1,9 @@
 import unittest
 from src.module.genetic_operations import generate_random_chromosome
+from src.module.genetic_operations import sort_and_select_top_solutions
 
 
-class TestSolutionGenerator(unittest.TestCase):
+class TestGeneticOperations(unittest.TestCase):
     def test_generate_random_chromosome(self):
         # Sample NODE_COORD_SECTION dictionary
         node_coords = {
@@ -24,6 +25,41 @@ class TestSolutionGenerator(unittest.TestCase):
         self.assertTrue(all(city_id in random_chromosome for city_id in node_coords.keys()))
         # Ensure all city IDs in the chromosome are unique
         self.assertEqual(len(set(random_chromosome)), num_cities)
+
+    def test_sort_and_select_top_solutions(self):
+        # Mocking chromosomes and tsp_data
+        chromosomes = [
+            [1, 2, 3, 4, 5],
+            [5, 4, 3, 2, 1],
+            [2, 3, 4, 1, 5],
+            [3, 4, 5, 1, 2]
+            # Add more chromosomes as needed
+        ]
+
+        # Mocking tsp_data (NODE_COORD_SECTION)
+        tsp_data = {
+            'NODE_COORD_SECTION': {
+                1: (565.0, 575.0),
+                2: (25.0, 185.0),
+                3: (345.0, 750.0),
+                4: (745.0, 225.0),
+                5: (475.0, 475.0)
+                # Add more city coordinates as needed
+            }
+        }
+
+        # Percentage of top solutions to select
+        percentage_to_select = 10  # Change this to your desired percentage
+
+        # Test the function
+        top_solutions = sort_and_select_top_solutions(chromosomes, tsp_data, percentage_to_select)
+
+        # Validate the output
+        expected_num_selected = len(chromosomes) * percentage_to_select // 100
+        self.assertEqual(len(top_solutions), expected_num_selected)
+        # Ensure the selected solutions are present in the original chromosomes
+        for solution in top_solutions:
+            self.assertIn(solution, chromosomes)
 
 
 if __name__ == '__main__':
