64f0d7932f766438d2e0e1324b027c490b48c3a6
CheesyChocolate <dev@behnamlal.xyz>
2023-11-19 19:14:46 +0000

    use `order_crossover` function in `core.py` module

--- src/core.py
+++ src/core.py
@@ -2,9 +2,8 @@ import sys
 
 from module.file_reader import read_tsp_file
 from module.mutation import generate_random_chromosome
-from module.selection import rank_selection
+from module.crossover import order_crossover
 from module.selection import roulette_selection
-from module.selection import tournament_selection
 from module.data_calculator import fitness
 from module.visualization import plot_tsp_cities
 
@@ -21,44 +20,40 @@ def main():
     # Read TSP data from the file
     tsp_data = read_tsp_file(tsp_file_path)
 
-    # Plot TSP nodes before generating random chromosome
-    plot_tsp_cities(tsp_data['NODE_COORD_SECTION'])
-
-    # Generate 20 random chromosomes and calculate their total distances
+    # Generate 100 random chromosomes and calculate their total distances
     chromosomes = []
     for _ in range(100):
         random_chromosome = generate_random_chromosome(tsp_data['NODE_COORD_SECTION'])
         chromosomes.append(random_chromosome)
 
-    # Find the 1 percent best chromosoms using tournament selection
-    best_chromosomes = rank_selection(chromosomes, tsp_data, 1)
-
-    # Print the best chromosomes and their total distances
-    for i in range(len(best_chromosomes)):
-        print("\nBest Solution", i + 1)
-        print("Path:", best_chromosomes[i])
-        print("Total Distance:", fitness(best_chromosomes[i], tsp_data['NODE_COORD_SECTION']))
-        # plot_tsp_cities(tsp_data['NODE_COORD_SECTION'], best_chromosomes[i])
-
     # find 5 chromosomes using roulette wheel selection
     selected_chromosomes = roulette_selection(chromosomes, tsp_data, 5)
 
-    # Print the selected chromosomes and their total distances
-    for i in range(len(selected_chromosomes)):
-        print("\nSelected Solution", i + 1)
-        print("Path:", selected_chromosomes[i])
-        print("Total Distance:", fitness(selected_chromosomes[i], tsp_data['NODE_COORD_SECTION']))
-        # plot_tsp_cities(tsp_data['NODE_COORD_SECTION'], selected_chromosomes[i])
+    # print parent 1
+    print("\nParent 1")
+    print("Path:", selected_chromosomes[0])
+    print("Total Distance:", fitness(selected_chromosomes[0], tsp_data['NODE_COORD_SECTION']))
 
-    # find 5 chromosomes using tournament selection
-    selected_chromosomes = tournament_selection(chromosomes, tsp_data, 5)
+    # print parent 2
+    print("\nParent 2")
+    print("Path:", selected_chromosomes[1])
+    print("Total Distance:", fitness(selected_chromosomes[1], tsp_data['NODE_COORD_SECTION']))
 
-    # Print the selected chromosomes and their total distances
-    for i in range(len(selected_chromosomes)):
-        print("\nSelected Solution", i + 1)
-        print("Path:", selected_chromosomes[i])
-        print("Total Distance:", fitness(selected_chromosomes[i], tsp_data['NODE_COORD_SECTION']))
-        # plot_tsp_cities(tsp_data['NODE_COORD_SECTION'], selected_chromosomes[i])
+    # perform order crossover
+    child1, child2 = order_crossover(selected_chromosomes[0], selected_chromosomes[1])
+
+    # print child 1
+    print("\nChild 1")
+    print("Path:", child1)
+    print("Total Distance:", fitness(child1, tsp_data['NODE_COORD_SECTION']))
+    plot_tsp_cities(tsp_data['NODE_COORD_SECTION'], child1)
+
+    # print child 2
+    print("\nChild 2")
+    print("Path:", child2)
+    print("Total Distance:", fitness(child2, tsp_data['NODE_COORD_SECTION']))
+    plot_tsp_cities(tsp_data['NODE_COORD_SECTION'], child2)
+
 
 if __name__ == "__main__":
     main()
