0b3d764a37c1c8c4213150203a8b720bf4954978
CheesyChocolate <dev@behnamlal.xyz>
2023-12-03 15:01:00 +0000

    add another unit test for ordered crossover

--- tests/test_crossover.py
+++ tests/test_crossover.py
@@ -23,6 +23,25 @@ class TestOrderCrossover(unittest.TestCase):
         self.assertNotEqual(child1, parent1)
         self.assertNotEqual(child2, parent2)
 
+    def test_order_crossover_open_loop(self):
+        # Test chromosomes
+        parent1 = [1, 2, 3, 4, 5, 6, 7, 8]
+        parent2 = [1, 7, 2, 4, 8, 5, 9, 6]
+
+        # Perform crossover
+        child1, child2 = order_crossover(parent1, parent2)
+
+        # Assert children are of the same length as parents
+        self.assertEqual(len(child1), len(parent1))
+        self.assertEqual(len(child2), len(parent2))
+
+        # Assert other genes in the children are unique
+        child1_no_1 = child1[1:]
+        child2_no_1 = child2[1:]
+        self.assertEqual(len(child1_no_1), len(set(child1_no_1)))
+        self.assertEqual(len(child2_no_1), len(set(child2_no_1)))
+        self.assertNotEqual(child1, parent1)
+        self.assertNotEqual(child2, parent2)
 
 if __name__ == '__main__':
     unittest.main()
