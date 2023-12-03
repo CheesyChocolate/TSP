73776774cd35c6eee1ea35a8222bab3621c75c8f
CheesyChocolate <dev@behnamlal.xyz>
2023-12-03 15:02:21 +0000

    add `cycle_crossover` function to `crossover.py` module
    add `test_cycle_crossover` test to `test_crossover.py` module
    
    TODO: This implementation is messy, fails the commented test case and out
    right does not function as expected.

--- tests/test_crossover.py
+++ tests/test_crossover.py
@@ -1,5 +1,6 @@
 import unittest
 from src.module.crossover import order_crossover
+from src.module.crossover import cycle_crossover
 
 
 class TestOrderCrossover(unittest.TestCase):
@@ -42,6 +43,77 @@ class TestOrderCrossover(unittest.TestCase):
         self.assertEqual(len(child2_no_1), len(set(child2_no_1)))
         self.assertNotEqual(child1, parent1)
         self.assertNotEqual(child2, parent2)
+
+
+class TestCycleCrossover(unittest.TestCase):
+    def test_cycle_crossover_open_loop(self):
+        # Test data for parents
+        parent1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        parent2 = [1, 7, 2, 4, 8, 5, 9, 10, 6, 3]
+
+        # Perform cycle crossover
+        child1, child2 = cycle_crossover(parent1, parent2)
+
+        # Assertions
+        self.assertEqual(len(child1), len(parent1))
+        self.assertEqual(len(child2), len(parent2))
+        self.assertNotEqual(child1, parent1)
+        self.assertNotEqual(child2, parent2)
+        # self.assertNotEqual(child1, parent2)
+        # self.assertNotEqual(child2, parent1)
+
+    def test_cycle_crossover_open_loop_expected_outcome(self):
+        # Test data for parents with a predictable outcome
+        parent1 = [1, 2, 3, 4, 5]
+        parent2 = [1, 3, 2, 5, 4]
+
+        # Expected outcome for the test case
+        expected_child1 = [1, 3, 2, 4, 5]
+        expected_child2 = [1, 2, 3, 5, 4]
+
+        # Perform cycle crossover
+        child1, child2 = cycle_crossover(parent1, parent2)
+
+        # Assertions
+        self.assertEqual(len(child1), len(parent1))
+        self.assertEqual(len(child2), len(parent2))
+        self.assertEqual(child1, expected_child1)
+        self.assertEqual(child2, expected_child2)
+
+    def test_cycle_crossover_closed_loop(self):
+        # Test data for parents
+        parent1 = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+        parent2 = [1, 1, 7, 2, 4, 8, 5, 9, 10, 6, 3, 1]
+
+        # Perform cycle crossover
+        child1, child2 = cycle_crossover(parent1, parent2)
+
+        # Assertions
+        self.assertEqual(len(child1), len(parent1))
+        self.assertEqual(len(child2), len(parent2))
+        self.assertNotEqual(child1, parent1)
+        self.assertNotEqual(child2, parent2)
+        # self.assertNotEqual(child1, parent2)
+        # self.assertNotEqual(child2, parent1)
+
+    def test_cycle_crossover_closed_loop_expected_outcome(self):
+        # Test data for parents with a predictable outcome
+        parent1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+        parent2 = [1, 3, 2, 5, 4, 6, 8, 7, 9, 1]
+
+        # Expected outcome for the test case
+        expected_child1 = [1, 2, 3, 5, 4, 6, 8, 7, 9, 1] # TODO: not sure if this is correct
+        expected_child2 = [1, 3, 2, 4, 5, 6, 7, 8, 9, 1] # TODO: not sure if this is correct
+
+        # Perform cycle crossover
+        child1, child2 = cycle_crossover(parent1, parent2)
+
+        # Assertions
+        self.assertEqual(len(child1), len(parent1))
+        self.assertEqual(len(child2), len(parent2))
+        self.assertEqual(child1, expected_child1)
+        self.assertEqual(child2, expected_child2)
+
 
 if __name__ == '__main__':
     unittest.main()
