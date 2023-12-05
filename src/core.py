8299b4cfcde39ec7529bd5b012df791405b601a6
CheesyChocolate <dev@behnamlal.xyz>
2023-12-05 18:21:37 +0000

    update `core.py` for a more addvanced algorithm

--- src/core.py
+++ src/core.py
@@ -5,9 +5,11 @@ import random
 from module.crossover import cycle_crossover
 from module.data_calculator import fitness
 from module.file_reader import read_tsp_file
+from module.local_search import two_opt as three_opt
 from module.local_search import partial_two_opt
 from module.mutation import generate_random_chromosome
-from module.mutation import swap_mutation
+from module.mutation import insert_mutation
+from module.mutation import random_slide_mutation
 from module.selection import rank_selection
 from module.selection import roulette_selection
 from module.visualization import plot_tsp_cities
@@ -20,8 +22,8 @@ def main():
         # Get the TSP file path from the arguments
         tsp_file_path = sys.argv[1]
     else:
-        # default to berlin52.tsp
-        tsp_file_path = "./data/ulysses22.tsp"
+        print("Please provide the TSP file path as an argument.")
+        print("Example: python3 core.py <TSP file path>")
 
     tsp_data = read_tsp_file(tsp_file_path)
 
@@ -36,6 +38,7 @@ def main():
     population = [generate_random_chromosome(coords) for _ in range(100)]
 
     best_chromosome = None
+    consecutive_same_solution_count = 0
 
     for generation in range(100):
         # get the best chromosome using rank selection
@@ -45,53 +48,69 @@ def main():
         print_chromosome(best_chromosome, fitness(best_chromosome, coords), generation)
 
         # Create the next generation
-        second_generation = [best_chromosome]
+        next_generation = [best_chromosome]
 
         # Generate new chromosomes for the second generation using mutation and crossover
         for _ in range(99):
-            # get the crossover probability randomly
-            crossover_probability = random.random()
+
+            # Initialize parent chromosomes
+            parent_chromosome1, parent_chromosome2 = None, None
 
             # apply crossover
-            if crossover_probability > 0.4:
-                # find 2 chromosomes using roulette wheel selection
-                selected_chromosomes = roulette_selection(population, tsp_data, 2)
-                # apply crossover
-                new_chromosome1, new_chromosome2 = cycle_crossover(selected_chromosomes[0], selected_chromosomes[1])
-                # add the crossover chromosomes to the second generation
-                second_generation.append(new_chromosome1)
-                second_generation.append(new_chromosome2)
-            # apply mutation
+            if random.random() < 0.5:
+                # use rand selection to find 2 chromosomes (50% chance)
+                parent_chromosome1, parent_chromosome2 = rank_selection(population, tsp_data, 2)
             else:
-                # find 1 chromosome using roulette wheel selection
-                selected_chromosomes = roulette_selection(population, tsp_data, 1)
-                # apply mutation
-                new_chromosome = swap_mutation(selected_chromosomes[0])
-                # apply 2-opt on the new chromosome
-                new_chromosome = partial_two_opt(new_chromosome, coords)
-                # add the mutation chromosome to the second generation
-                second_generation.append(new_chromosome)
+                # use roulette wheel selection to find 2 chromosomes (50% chance)
+                parent_chromosome1, parent_chromosome2 = roulette_selection(population, tsp_data, 2)
+
+                # Crossover using order crossover
+                new_chromosome1, new_chromosome2 = cycle_crossover(parent_chromosome1, parent_chromosome2)
+                next_generation.append(new_chromosome1)
+                next_generation.append(new_chromosome2)
+
+                if random.random() < 0.5:
+                    # 50% chance of using insert mutation
+                    if random.random() < 0.5:
+                        # use insert mutation
+                        new_chromosome = insert_mutation(parent_chromosome1)
+                        # apply partial 2-opt on the new chromosome
+                        new_chromosome = partial_two_opt(new_chromosome, coords)
+                    else:
+                        # use random slide mutation
+                        new_chromosome = random_slide_mutation(parent_chromosome1)
+                        # apply partial 2-opt on the new chromosome
+                        new_chromosome = partial_two_opt(new_chromosome, coords)
+
+                    # add the mutation chromosome to the second generation
+                    next_generation.append(new_chromosome)
 
         # keep only the best 100 chromosomes
-        second_generation = rank_selection(second_generation, tsp_data, 100)
+        next_generation = rank_selection(next_generation, tsp_data, 100)
+
+        # check for termination criteria
+        # 1. 100 generations passed
+        # 2. 5 consecutive same solutions
+        if best_chromosome == next_generation[0]:
+            consecutive_same_solution_count += 1
+        else:
+            consecutive_same_solution_count = 0
+
+        if consecutive_same_solution_count == 5:
+            break
 
         # Replace the current population with the next generation
-        population = second_generation
+        population = next_generation
 
     # Plot the best chromosome of the last generation
     plot_tsp_cities(coords, best_chromosome)
-
-    # print the best chromosome
     print_chromosome(best_chromosome, fitness(best_chromosome, coords))
 
-    # 2-opt on the best chromosome
-    print("Applying 2-opt...")
-    best_chromosome = partial_two_opt(best_chromosome, coords)
+    # apply 3-opt on the best chromosome
+    best_chromosome = three_opt(best_chromosome, coords)
 
-    # Plot the best chromosome after 2-opt
+    # Plot the best chromosome after applying 3-opt
     plot_tsp_cities(coords, best_chromosome)
-
-    # print the best chromosome
     print_chromosome(best_chromosome, fitness(best_chromosome, coords))
 
 
