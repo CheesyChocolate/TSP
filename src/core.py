2c3a97dd37ade34cc0702af543c79d4be49e2bfc
CheesyChocolate <dev@behnamlal.xyz>
2023-12-14 08:17:25 +0000

    rewrite the `core.py` file according to changes in the modules

--- src/core.py
+++ src/core.py
@@ -3,14 +3,17 @@ import sys
 import random
 
 from module.crossover import cycle_crossover
+from module.data_calculator import calculate_distance_matrix
 from module.data_calculator import fitness
+from module.data_calculator import combine_population_fitness
 from module.file_reader import read_tsp_file
-from module.local_search import three_opt
-from module.local_search import partial_two_opt
+from module.local_search import two_opt as three_opt
+from module.local_search import two_opt
 from module.mutation import generate_random_chromosome
 from module.mutation import insert_mutation
 from module.mutation import random_slide_mutation
-from module.selection import rank_selection
+from module.selection import elitist_selection
+from module.selection import elitist_selection as rank_selection
 from module.selection import roulette_selection
 from module.visualization import plot_tsp_cities
 from module.visualization import print_chromosome
@@ -29,92 +32,86 @@ def main():
     tsp_data = read_tsp_file(tsp_file_path)
 
     # get the coords from the tsp data
-    coords = tsp_data['NODE_COORD_SECTION']
+    node_cords = tsp_data['NODE_COORD_SECTION']
+
+    # calculate the distance matrix
+    distance_matrix = calculate_distance_matrix(node_cords)
 
     # plot the cities on a graph
-    plot_tsp_cities(coords)
+    plot_tsp_cities(node_cords)
 
-    # Generate 100 random chromosomes and calculate their total distances
     # Create the initial population
-    population = [generate_random_chromosome(coords) for _ in range(100)]
+    population = [generate_random_chromosome(node_cords) for _ in range(100)]
 
     best_chromosome = None
     consecutive_same_solution_count = 0
 
     for generation in range(100):
-        # get the best chromosome using rank selection
-        best_chromosome = rank_selection(population, tsp_data, 1)[0]
+
+        # Combine population with their respective fitness scores
+        combined_data = combine_population_fitness(population, distance_matrix)
+
+        # find the best chromosome with elitist selection
+        best_chromosome = elitist_selection(combined_data, 1)[0]
+        best_fitness = fitness(best_chromosome, distance_matrix)
 
         # print the best chromosome
-        print_chromosome(best_chromosome, fitness(best_chromosome, coords), generation)
+        print_chromosome(best_chromosome, best_fitness, generation)
 
-        # Create the next generation
-        next_generation = [best_chromosome]
+        # create the next generation
+        next_population = [best_chromosome]
 
         # Generate new chromosomes for the second generation using mutation and crossover
-        for _ in range(99):
+        while len(next_population) < len(population):
 
-            # Initialize parent chromosomes
-            parent_chromosome1, parent_chromosome2 = None, None
-
-            # apply crossover
+            # selection
             if random.random() < 0.5:
-                # use rand selection to find 2 chromosomes (50% chance)
-                parent_chromosome1, parent_chromosome2 = rank_selection(population, tsp_data, 2)
+                # rank selection
+                parent1, parent2 = rank_selection(combined_data, 2)
             else:
-                # use roulette wheel selection to find 2 chromosomes (50% chance)
-                parent_chromosome1, parent_chromosome2 = roulette_selection(population, tsp_data, 2)
+                # roulette selection
+                parent1, parent2 = roulette_selection(combined_data, 2)
 
-                # Crossover using order crossover
-                new_chromosome1, new_chromosome2 = cycle_crossover(parent_chromosome1, parent_chromosome2)
-                next_generation.append(new_chromosome1)
-                next_generation.append(new_chromosome2)
+            # crossover using cycle crossover
+            child1, child2 = cycle_crossover(parent1, parent2)
+            next_population.append(child1)
+            next_population.append(child2)
 
-                if random.random() < 0.5:
-                    # 50% chance of using insert mutation
-                    if random.random() < 0.5:
-                        # use insert mutation
-                        new_chromosome = insert_mutation(parent_chromosome1)
-                        # apply partial 2-opt on the new chromosome
-                        new_chromosome = partial_two_opt(new_chromosome, coords)
-                    else:
-                        # use random slide mutation
-                        new_chromosome = random_slide_mutation(parent_chromosome1)
-                        # apply partial 2-opt on the new chromosome
-                        new_chromosome = partial_two_opt(new_chromosome, coords)
+            if random.random() < 0.5:
+                child1 = insert_mutation(child1)
+                child1 = two_opt(child1, distance_matrix)
+            else:
+                child1 = random_slide_mutation(child1)
+                child1 = two_opt(child1, distance_matrix)
 
-                    # add the mutation chromosome to the second generation
-                    next_generation.append(new_chromosome)
+            # add the mutated child to the next population
+            next_population.append(child1)
 
-        # keep only the best 100 chromosomes
-        next_generation = rank_selection(next_generation, tsp_data, 100)
+            # check for termination condition
+            if best_chromosome == next_population[0]:
+                consecutive_same_solution_count += 1
+            else:
+                consecutive_same_solution_count = 0
 
-        # check for termination criteria
-        # 1. 100 generations passed
-        # 2. 5 consecutive same solutions
-        if best_chromosome == next_generation[0]:
-            consecutive_same_solution_count += 1
-        else:
-            consecutive_same_solution_count = 0
+            if consecutive_same_solution_count == 5:
+                break
 
-        if consecutive_same_solution_count == 5:
-            break
+            # replace the population with the new generation
+            population = next_population
 
-        # Replace the current population with the next generation
-        population = next_generation
+    # Plot the best chromosome of the final generation
+    plot_tsp_cities(node_cords, best_chromosome)
+    print_chromosome(best_chromosome, best_fitness)
 
-    # Plot the best chromosome of the last generation
-    plot_tsp_cities(coords, best_chromosome)
-    print_chromosome(best_chromosome, fitness(best_chromosome, coords))
+    print("Applyong 3-opt on the best chromosome...")
 
-    print("Applying 3-opt on the best chromosome...")
+    # Apply 3-opt on the best chromosome
+    best_chromosome = three_opt(best_chromosome, distance_matrix)
 
-    # apply 3-opt on the best chromosome
-    best_chromosome = three_opt(best_chromosome, coords)
+    # Plot the best chromosome of the final generation
+    plot_tsp_cities(node_cords, best_chromosome)
+    print_chromosome(best_chromosome, fitness(best_chromosome, distance_matrix))
 
-    # Plot the best chromosome after applying 3-opt
-    plot_tsp_cities(coords, best_chromosome)
-    print_chromosome(best_chromosome, fitness(best_chromosome, coords))
 
 
 if __name__ == "__main__":
